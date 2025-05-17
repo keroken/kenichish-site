c60da1ce131de88682748a174cda08a7b9ddae0c
keroken <kenichi.shinagawa@gmail.com>
2025-05-18 01:49:38 +0900

    basic styling for title and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,24 @@ function App() {
 
   return (
     <div className={styles.root}>
-      <h1>KENICHI SHINAGAWA</h1>
+      <h1 className={styles.title}>KENICHI SHINAGAWA</h1>
+      <ul className={styles.mainNav}>
+        <li>
+          <a href="">Architecture</a>
+        </li>
+        <li>
+          <a href="">Web/IOT</a>
+        </li>
+        <li>
+          <a href="">Interactive</a>
+        </li>
+        <li>
+          <a href="">Graphic</a>
+        </li>
+        <li>
+          <a href="">Others</a>
+        </li>
+      </ul>
     </div>
   )
 }
