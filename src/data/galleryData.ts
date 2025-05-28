b2314a973f61032abc9e1b941f5e283c19e8343b
keroken <kenichi.shinagawa@gmail.com>
2025-05-28 09:59:06 +0900

    add gallery data

---
+++ src/data/galleryData.ts
@@ -0,0 +1,61 @@
+export const galleryData = [
+  {
+    title: "Motel Project",
+    description: "A conceptual image of the Motel Project.",
+    src: "images/motel_project_02.jpg",
+    alt: "Motel Project Concept",
+  },
+  {
+    title: "Motel Project",
+    description: "A conceptual image of the Motel Project.",
+    src: "images/motel_project_01.jpg",
+    alt: "Motel Project Concept",
+  },
+  {
+    title: "Music School Project",
+    description: "A conceptual object of the Music School Project.",
+    src: "images/music_school_project_00.jpg",
+  },
+  {
+    title: "Music School Project",
+    description: "A conceptual object of the Music School Project.",
+    src: "images/music_school_project_01.jpg",
+    alt: "Music School Project Concept",
+  },
+  {
+    title: "Music School Project",
+    description: "A conceptual object of the Music School Project.",
+    src: "images/music_school_project_02.jpg",
+    alt: "Music School Project Concept",
+  },
+  {
+    title: "Music School Project",
+    description: "A conceptual object of the Music School Project.",
+    src: "images/music_school_project_03.jpg",
+    alt: "Music School Project Concept",
+  },
+  {
+    title: "Music School Project",
+    description: "A conceptual object of the Music School Project.",
+    src: "images/music_school_project_04.jpg",
+    alt: "Music School Project Concept",
+  },
+  {
+    title: "Music School Project",
+    description: "A conceptual object of the Music School Project.",
+    src: "images/music_school_project_05.jpg",
+    alt: "Music School Project Concept",
+  },
+  {
+    title: "Music School Project",
+    description: "A conceptual object of the Music School Project.",
+    src: "images/music_school_project_06.jpg",
+    alt: "Music School Project Concept",
+  },
+  {
+    title: "Music School Project",
+    description: "A conceptual object of the Music School Project.",
+    src: "images/music_school_project_07.jpg",
+    alt: "Music School Project Concept",
+  },
+];
